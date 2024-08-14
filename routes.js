d3225f6291de6d8cedc3c8d20e0ab0051b1dfb50
Mateus Caetano <mateuscaetano952@gmail.com>
2024-08-14 10:01:07 -0300

    bootstrap navbar e main page

--- routes.js
+++ routes.js
@@ -5,10 +5,6 @@ const contatoController = require('./src/controllers/contatoController');
 
 // Rotas da home
 route.get('/', homeController.paginaInicial);
-route.post('/', homeController.trataPost);
-
-// Rotas de contato
-route.get('/contato', contatoController.paginaInicial);
 
 
 module.exports = route;
